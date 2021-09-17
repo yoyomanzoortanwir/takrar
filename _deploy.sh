7249dfda70311cf95a211f7d7290275db180f2bf
yoyomanzoor <smanzoor@uchicago.edu>
2021-09-16 22:27:13 -0500

    update travis

--- _deploy.sh
+++ _deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "main" ] && exit 0
 
 git config --global user.email "smanzoor@uchicago.edu"
 git config --global user.name "yoyomanzoor"
@@ -13,4 +13,4 @@ cd book-output
 cp -r ../_book/* ./
 git add --all *
 git commit -m"Update the book" || true
-git push -q origin gh-pages
+git push -q origin main
