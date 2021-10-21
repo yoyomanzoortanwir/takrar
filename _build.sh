f4c4bbd969a32948abe09bb2bc5c9a3e8203bbd9
yoyomanzoortanwir <smanzoor@uchicago.edu>
2021-10-20 19:00:08 -0500

    update stuff

--- _build.sh
+++ _build.sh
@@ -2,6 +2,7 @@
 
 set -ev
 
+R -e 'install.packages(c("ggplot2", "ggpubr", "magrittr", "dplyr", "MASS", "kableExtra"))'
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
